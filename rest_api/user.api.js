3d31292ace64fd1c1ef96e1bec0c30033cf16c80
khanhnguyend <khanh.nguyenduy@harveynash.vn>
2018-08-07 00:18:12 +0700

    Refactor code connect db by knexjs

--- rest_api/user.api.js
+++ rest_api/user.api.js
@@ -4,16 +4,15 @@ var userlogic = require('./user_logic');
 
 const test_account = { username: 'kevin', password: '123456'};
 
-login = (req, res) => {
+login = async(req, res) =>{
     var postdata = req.body;
-    userlogic.getUserByUsername(postdata.username).then(data => {
-        if(postdata.username == data.username && postdata.password == data.password){
-            res.status(200).send('login susscessful!');
-        }
-        else{
-            res.status(500).send('login fail!')
-        }
-    }); 
+    const user = await userlogic.getUserByUsername(postdata.username);
+    if(postdata.username == user.username && postdata.password == user.password){
+        res.status(200).send('login susscessful!');
+    }
+    else{
+        res.status(500).send('login fail!')
+    }
 }
 
 user_router.post('/login', login);
--- rest_api/user_logic.js
+++ rest_api/user_logic.js
@@ -2,26 +2,15 @@ var config = require('config');
 var db = require('knex')(config.get('db'));
 
 function getUserByUsername(username){
-    return db.raw(`SELECT username, password FROM nodejs_express.user WHERE username = '${username}'`)
-    .then(function (data) {
-        return data.rows[0];
-    })
-    .catch(function (e) { // Catch query err
-        console.log('error:', e);
-        exit();
+    var data = {};
+    return db('nodejs_express.user')
+    .select('username', 'password')
+    .where('username','=', username)
+    .then(rows => {
+        return rows[0];
     });
-}
 
-function exit(){
-    db.destroy();
-    process.exit();
 }
-
-// To handle unhandled Rejection
-process.on('unhandledRejection', function(err, promise) {
-    console.error('Unhandled rejection (promise: ', promise, ', reason: ', err, ').');
-    exit();
-});
 
 
 module.exports ={
